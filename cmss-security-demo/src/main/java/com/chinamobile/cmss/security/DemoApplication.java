7d9b0e15bc2ccecfc3a23b99b0e730edebaa885d
江平平 <jiangpingping@cmss.chinamobile.com>
2019-03-01 19:08:30 +0800

    初始化项目

---
+++ cmss-security-demo/src/main/java/com/chinamobile/cmss/security/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.chinamobile.cmss.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot应用启动类
+ *
+ * @author jiangpingping
+ * @date 2019-03-01 17:20
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
